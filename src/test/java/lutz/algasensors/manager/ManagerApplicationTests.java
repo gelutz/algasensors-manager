1866a9519af0472bfabd34ea3dbb3e1eb7acaa80
gelutz <gabriel.lutz5@gmail.com>
2025-10-03 14:25:45 -0300

    feat: movendo pastas

---
+++ src/test/java/lutz/algasensors/manager/ManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package lutz.algasensors.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
