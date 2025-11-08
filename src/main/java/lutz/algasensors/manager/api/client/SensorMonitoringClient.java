05a3c812048791533adb5d211fec85e75169c211
gelutz <gabriel.lutz5@gmail.com>
2025-11-08 11:02:31 -0300

    feat: adicionando forma diferente de usar o restclient

--- src/main/java/lutz/algasensors/manager/api/client/SensorMonitoringClient.java
+++ src/main/java/lutz/algasensors/manager/api/client/SensorMonitoringClient.java
@@ -2,11 +2,20 @@ package lutz.algasensors.manager.api.client;
 
 import io.hypersistence.tsid.TSID;
 import lutz.algasensors.manager.api.model.SensorMonitoringOutput;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.service.annotation.DeleteExchange;
+import org.springframework.web.service.annotation.GetExchange;
+import org.springframework.web.service.annotation.HttpExchange;
+import org.springframework.web.service.annotation.PutExchange;
 
+@HttpExchange("/api/sensors/{sensorId}/monitoring")
 public interface SensorMonitoringClient {
-	SensorMonitoringOutput getMonitoringDetails(TSID sensorId);
+	@GetExchange
+	SensorMonitoringOutput getMonitoringDetails(@PathVariable TSID sensorId);
 
-	void enableMonitoring(TSID sensorId);
+	@DeleteExchange("/enable")
+	void enableMonitoring(@PathVariable TSID sensorId);
 
-	void disableMonitoring(TSID sensorId);
+	@PutExchange("/enable")
+	void disableMonitoring(@PathVariable TSID sensorId);
 }
