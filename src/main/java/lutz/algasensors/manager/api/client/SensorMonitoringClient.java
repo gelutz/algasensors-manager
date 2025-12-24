bf834192f30c9c8a3b5b26b1cef8f2334147fdbd
gelutz <gabriel.lutz5@gmail.com>
2025-12-23 23:07:26 -0300

    saving progress

--- src/main/java/lutz/algasensors/manager/api/client/SensorMonitoringClient.java
+++ src/main/java/lutz/algasensors/manager/api/client/SensorMonitoringClient.java
@@ -1,12 +1,13 @@
 package lutz.algasensors.manager.api.client;
 
-import io.hypersistence.tsid.TSID;
-import lutz.algasensors.manager.api.model.SensorMonitoringOutput;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.service.annotation.DeleteExchange;
 import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.HttpExchange;
 import org.springframework.web.service.annotation.PutExchange;
+
+import io.hypersistence.tsid.TSID;
+import lutz.algasensors.manager.api.model.SensorMonitoringOutput;
 
 @HttpExchange("/api/sensors/{sensorId}/monitoring")
 public interface SensorMonitoringClient {
